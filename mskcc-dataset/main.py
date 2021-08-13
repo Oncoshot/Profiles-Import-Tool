fa88787296c803cde47b39ff1ce708bd7598e026
Ruslan Enikeev <ruslan@oncoshot.com>
2021-08-13 13:14:00 +0800

    ONC-4713 clean up

--- mskcc-dataset/main.py
+++ mskcc-dataset/main.py
@@ -19,11 +19,9 @@ mutation = pd.read_csv('mskcc-dataset/source-data/data_mutations_mskcc.txt',
                        usecols=['Hugo_Symbol', 'HGVSp_Short', 'Tumor_Sample_Barcode'])
 
 # Pre-Processing
-mutation[['temp', 'HGVSp_Short']] = mutation['HGVSp_Short'].str.split(
-    ".", expand=True)
+mutation[['temp', 'Alteration']] = mutation['HGVSp_Short'].str.split(".", expand=True) #get Alteration  
 
-mutation['MUTATION'] = mutation['Hugo_Symbol'] + \
-    ": " + mutation['HGVSp_Short']
+mutation['MUTATION'] = mutation['Hugo_Symbol'] + ": " + mutation['Alteration']
 
 # Combine mutation and samples
 combined_mutations = pd.merge(
@@ -37,22 +35,21 @@ combined_mutations = pd.merge(
 
 combined = pd.merge(patients, combined_mutations, on=[
                     'PATIENT_ID']).groupby(by=["PATIENT_ID"]).agg({
-                        'PATIENT_ID': lambda x: x.iloc[0],
                         "SEX": lambda x: x.iloc[0],
-                        'CANCER_TYPE': lambda x: list(x),
-                        'CANCER_TYPE_DETAILED': lambda x: list(x),
-                        'METASTATIC_SITE': lambda x: list(x),
-                        'MUTATION': 'sum'
+                        'CANCER_TYPE': lambda x: list(set(x)),
+                        'CANCER_TYPE_DETAILED': lambda x: list(set(x)),
+                        'METASTATIC_SITE': lambda x: list(set(x)),
+                        'MUTATION': lambda x: list(set([item for sublist in x for item in sublist]))
                     })
-combined['CANCER_TYPE'] = combined['CANCER_TYPE'] + \
-    combined['CANCER_TYPE_DETAILED']
 
-combined = combined.drop(columns=["CANCER_TYPE_DETAILED"])
+combined['CANCER'] = combined['CANCER_TYPE'] + combined['CANCER_TYPE_DETAILED']
+combined = combined.drop(columns=['CANCER_TYPE', "CANCER_TYPE_DETAILED"])
+
 # Output to CSV
 combined.to_csv('mskcc-dataset/patients.csv')
 
 # Processing for JSON
-combined.rename(columns={'CANCER_TYPE': 'diagnoses_Cancer_Unstructured',
+combined.rename(columns={'CANCER': 'diagnoses_Cancer_Unstructured',
                          'SEX': 'gender_Unstructured',
                          'METASTATIC_SITE': 'organInvolved_Unstructured',
                          'MUTATION': 'mutations_Unstructured'
