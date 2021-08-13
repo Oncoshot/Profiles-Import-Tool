b0b739a4b07b8946c5c4cd60d20f02b582777648
angnobel <nobelang78@gmail.com>
2021-08-13 12:30:32 +0800

    ONC-4713: Add Mutations

--- mskcc-dataset/main.py
+++ mskcc-dataset/main.py
@@ -13,10 +13,37 @@ samples = pd.read_csv('mskcc-dataset/source-data/data_clinical_sample.txt',
                       header=4,
                       usecols=['PATIENT_ID', 'SAMPLE_ID', 'CANCER_TYPE', 'CANCER_TYPE_DETAILED', 'METASTATIC_SITE'])
 
+mutation = pd.read_csv('mskcc-dataset/source-data/data_mutations_mskcc.txt',
+                       sep="\t",
+                       header=1,
+                       usecols=['Hugo_Symbol', 'HGVSp_Short', 'Tumor_Sample_Barcode'])
+
 # Pre-Processing
-combined = pd.merge(patients, samples, on=[
-                    'PATIENT_ID', 'PATIENT_ID']).groupby(by=["PATIENT_ID"]).agg({
-                        'PATIENT_ID': lambda x: x.iloc[0], "SEX": lambda x: x.iloc[0], 'CANCER_TYPE': lambda x: list(x), 'CANCER_TYPE_DETAILED': lambda x: list(x), 'METASTATIC_SITE': lambda x: list(x)}).drop(columns=['PATIENT_ID'])
+mutation[['temp', 'HGVSp_Short']] = mutation['HGVSp_Short'].str.split(
+    ".", expand=True)
+
+mutation['MUTATION'] = mutation['Hugo_Symbol'] + \
+    ": " + mutation['HGVSp_Short']
+
+# Combine mutation and samples
+combined_mutations = pd.merge(
+    samples, mutation, left_on=['SAMPLE_ID'], right_on=["Tumor_Sample_Barcode"]).groupby(by=["SAMPLE_ID"]).agg({
+        'PATIENT_ID': lambda x: x.iloc[0],
+        'METASTATIC_SITE': lambda x: x.iloc[0],
+        'CANCER_TYPE': lambda x: x.iloc[0],
+        'CANCER_TYPE_DETAILED': lambda x: x.iloc[0],
+        'MUTATION': lambda x: list(x)
+    })
+
+combined = pd.merge(patients, combined_mutations, on=[
+                    'PATIENT_ID']).groupby(by=["PATIENT_ID"]).agg({
+                        'PATIENT_ID': lambda x: x.iloc[0],
+                        "SEX": lambda x: x.iloc[0],
+                        'CANCER_TYPE': lambda x: list(x),
+                        'CANCER_TYPE_DETAILED': lambda x: list(x),
+                        'METASTATIC_SITE': lambda x: list(x),
+                        'MUTATION': 'sum'
+                    })
 combined['CANCER_TYPE'] = combined['CANCER_TYPE'] + \
     combined['CANCER_TYPE_DETAILED']
 
@@ -27,7 +54,8 @@ combined.to_csv('mskcc-dataset/patients.csv')
 # Processing for JSON
 combined.rename(columns={'CANCER_TYPE': 'diagnoses_Cancer_Unstructured',
                          'SEX': 'gender_Unstructured',
-                         'METASTATIC_SITE': 'organInvolved_Unstructured'
+                         'METASTATIC_SITE': 'organInvolved_Unstructured',
+                         'MUTATION': 'mutations_Unstructured'
                          }, inplace=True)
 combined = combined.rename_axis("id")
 combined['referenceNo'] = combined.index.get_level_values(level='id').tolist()
